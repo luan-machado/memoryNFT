961d9296016765f0976f4b22b0e341ecc777b3f3
luan-machado <luan@sway.com.br>
2021-09-21 22:06:01 -0300

    Memory Game

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,191 @@ import Web3 from 'web3'
 import './App.css';
 import MemoryToken from '../abis/MemoryToken.json'
 import brain from '../brain.png'
+import pack from '../blank.png'
+
+const CARD_ARRAY = [
+  {
+    name: 'fries',
+    img: '/images/fries.png'
+  },
+  {
+    name: 'cheeseburger',
+    img: '/images/cheeseburger.png'
+  },
+  {
+    name: 'ice-cream',
+    img: '/images/ice-cream.png'
+  },
+  {
+    name: 'pizza',
+    img: '/images/pizza.png'
+  },
+  {
+    name: 'milkshake',
+    img: '/images/milkshake.png'
+  },
+  {
+    name: 'hotdog',
+    img: '/images/hotdog.png'
+  },
+  {
+    name: 'fries',
+    img: '/images/fries.png'
+  },
+  {
+    name: 'cheeseburger',
+    img: '/images/cheeseburger.png'
+  },
+  {
+    name: 'ice-cream',
+    img: '/images/ice-cream.png'
+  },
+  {
+    name: 'pizza',
+    img: '/images/pizza.png'
+  },
+  {
+    name: 'milkshake',
+    img: '/images/milkshake.png'
+  },
+  {
+    name: 'hotdog',
+    img: '/images/hotdog.png'
+  }
+]
 
 class App extends Component {
 
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+    this.setState({ cardArray: CARD_ARRAY.sort(() => 0.5 - Math.random()) })
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    // Load smart contract
+    const networkId = await web3.eth.net.getId()
+    const networkData = MemoryToken.networks[networkId]
+    if(networkData) {
+      const abi = MemoryToken.abi
+      const address = networkData.address
+      const token = new web3.eth.Contract(abi, address)
+      this.setState({ token })
+      const totalSupply = await token.methods.totalSupply().call()
+      this.setState({ totalSupply })
+      // Load Tokens
+      let balanceOf = await token.methods.balanceOf(accounts[0]).call()
+      for (let i = 0; i < balanceOf; i++) {
+        let id = await token.methods.tokenOfOwnerByIndex(accounts[0], i).call()
+        let tokenURI = await token.methods.tokenURI(id).call()
+        this.setState({
+          tokenURIs: [...this.state.tokenURIs, tokenURI]
+        })
+      }
+    } else {
+      alert('Smart contract not deployed to detected network.')
+    }
+  }
+
+  getImage = (cardId) => {
+    cardId = cardId.toString()
+    return CARD_ARRAY[cardId].img
+  }
+
+  openPack = () => {
+    var newCards = [...this.state.newCards];
+    for (var i = 0; i < 5; i++) {
+      newCards.push(this.getRandomCard());
+
+    }
+    this.setState({ newCards });
+    setTimeout(
+      () => this.addToColection(), 
+      1000
+    );
+  }
+  
+  getRandomCard = () => {
+    var image = CARD_ARRAY.sort(() => 0.5 - Math.random())[0]
+    return image;
+  }
+
+  addToColection = () => {
+    this.state.newCards.map((card, key) => {
+      this.state.token.methods.mint(
+        this.state.account,
+        window.location.origin + CARD_ARRAY[key].img.toString()
+      )
+      .send({ from: this.state.account })
+      .on('transactionHash', (hash) => {
+
+      })
+    })
+    console.log(this.state.newCards)
+  }
+
+
+  checkForMatch = async () => {
+    const optionOneId = this.state.cardsChosenId[0]
+    const optionTwoId = this.state.cardsChosenId[1]
+
+    if(optionOneId == optionTwoId) {
+      alert('You have clicked the same image!')
+    } else if (this.state.cardsChosen[0] === this.state.cardsChosen[1]) {
+      alert('You found a match')
+      this.state.token.methods.mint(
+        this.state.account,
+        window.location.origin + CARD_ARRAY[optionOneId].img.toString()
+      )
+      .send({ from: this.state.account })
+      .on('transactionHash', (hash) => {
+        this.setState({
+          cardsWon: [...this.state.cardsWon, optionOneId, optionTwoId],
+          tokenURIs: [...this.state.tokenURIs, CARD_ARRAY[optionOneId].img]
+        })
+      })
+    } else {
+      alert('Sorry, try again')
+    }
+    this.setState({
+      cardsChosen: [],
+      cardsChosenId: []
+    })
+    if (this.state.cardsWon.length === CARD_ARRAY.length) {
+      alert('Congratulations! You found them all!')
+    }
+  }
 
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      account: '0x0',
+      token: null,
+      totalSupply: 0,
+      newCards: [],
+      cardsOwn: [],
+      tokenURIs: [],
+      cardArray: [],
+      cardsChosen: [],
+      cardsChosenId: [],
+      cardsWon: [],
     }
   }
 
@@ -36,27 +213,24 @@ class App extends Component {
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <h1 className="d-4">Edit this file in App.js!</h1>
 
+
+            <div className="content mr-auto ml-auto">
+                <h1 className="d-4">Open Packs</h1>
                 <div className="grid mb-4" >
-
-                  {/* Code goes here... */}
-
+                  <img src={pack} data-id="openPack" onClick={this.openPack}/>
                 </div>
-
                 <div>
-
-                  {/* Code goes here... */}
-
-                  <div className="grid mb-4" >
-
-                    {/* Code goes here... */}
-
-                  </div>
-
+                { this.state.newCards.map((card, key) => {
+                    return(
+                      <img
+                        key={key}
+                        src={this.getImage(key)}
+                        data-id={key}
+                      />
+                    )
+                  })}
                 </div>
-
               </div>
 
             </main>
--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const MemoryToken = artifacts.require('./MemoryToken.sol')
 
 require('chai')
@@ -5,5 +7,65 @@ require('chai')
   .should()
 
 contract('Memory Token', (accounts) => {
-  // code goes here...
-})
+  let token
+
+  before(async () => {
+    token = await MemoryToken.deployed()
+  })
+
+  describe("deployed", async () => {
+    it("Success", async () => {
+      const address = token.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, "")
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+    it("HAs name", async () => {
+      const name = await token.name()
+      assert.equal(name, "Luan Token")
+    })
+    it("HAs Symbol", async () => {
+      const symbol = await token.symbol()
+      assert.equal(symbol, "L$N")
+    })
+  })
+
+  describe("Token DIstribution", async () => {
+    let result
+
+    it("MInts TOken", async () => {
+      await token.mint(accounts[0], "https://i.picsum.photos/id/972/200/300.jpg?hmac=UMf5f6BV9GkLiz0Xz9kMwm1riiTtlpIG2jt0WrxZ51Q")
+      
+      //increase total supply
+      result = await token.totalSupply()
+      assert.equal(result.toString(), '1', 'TOtal Supply is ok')
+
+      //increment owner balance
+      result = await token.balanceOf(accounts[0])
+      assert.equal(result.toString(), '1', 'balance of is ok')
+
+      //token should belong to owner
+      result = await token.ownerOf(1)
+      assert.equal(result.toString(), accounts[0].toString(), 'owner of is ok')
+      result = await token.tokenOfOwnerByIndex(accounts[0], 0)
+
+      // Owner see al tokens
+      let balanceOf = await token.balanceOf(accounts[0])
+      let tokenIds = []
+      for(let i = 0; i < balanceOf; i++){
+        let id = await token.tokenOfOwnerByIndex(accounts[0], i)
+        tokenIds.push(id.toString());
+      }
+
+      let expected = ['1']
+      assert.equal(tokenIds.toString(), expected.toString(), "Token id are correct")
+
+      // TOken URI correct
+      let tokenURI = await token.tokenURI('1')
+      assert.equal(tokenURI, "https://i.picsum.photos/id/972/200/300.jpg?hmac=UMf5f6BV9GkLiz0Xz9kMwm1riiTtlpIG2jt0WrxZ51Q")
+
+    })
+  })
+ 
+})  
